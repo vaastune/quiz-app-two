8f96b7964d55cbf674c4b7b7385535928f8c4366
Ruth <113599167+vaastune@users.noreply.github.com>
2024-11-29 14:48:53 +0700

    c

--- resources/views/quizzes/create.blade.php
+++ resources/views/quizzes/create.blade.php
@@ -3,9 +3,11 @@
 @section('title', 'Create Quiz')
 
 @section('content')
+<a href="{{ route('quizzes.create') }}" class="btn btn-primary">Create Quiz</a>
+
 <div class="container">
     <h1>Create a New Quiz</h1>
-    <form method="POST" action="{{ route('quizzes.store') }}">
+    <form method="POST" action="#">
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Quiz Title</label>
@@ -15,65 +17,3 @@
     </form>
 </div>
 @endsection
-
-
-
-
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <h1>Create a New Quiz</h1>
-    <form action="{{ route('quizzes.store') }}" method="POST">
-        @csrf
-        <div class="mb-3">
-            <label for="title" class="form-label">Quiz Title</label>
-            <input type="text" name="title" id="title" class="form-control" required>
-        </div>
-        <div id="questions">
-            <h3>Questions</h3>
-            <div class="question mb-3">
-                <label for="questions[0][text]" class="form-label">Question Text</label>
-                <input type="text" name="questions[0][text]" class="form-control" required>
-                <label for="questions[0][options][]" class="form-label">Options</label>
-                <input type="text" name="questions[0][options][]" class="form-control" required>
-                <input type="text" name="questions[0][options][]" class="form-control" required>
-                <label for="questions[0][correct_answer]" class="form-label">Correct Option (0 or 1)</label>
-                <input type="number" name="questions[0][correct_answer]" class="form-control" required>
-            </div>
-        </div>
-        <button type="button" id="add-question" class="btn btn-secondary">Add Question</button>
-        <button type="submit" class="btn btn-primary">Create Quiz</button>
-    </form>
-</div>
-
-<script>
-    document.getElementById('add-question').addEventListener('click', () => {
-        const questionCount = document.querySelectorAll('.question').length;
-        const newQuestion = `
-            <div class="question mb-3">
-                <label for="questions[${questionCount}][text]" class="form-label">Question Text</label>
-                <input type="text" name="questions[${questionCount}][text]" class="form-control" required>
-                <label for="questions[${questionCount}][options][]" class="form-label">Options</label>
-                <input type="text" name="questions[${questionCount}][options][]" class="form-control" required>
-                <input type="text" name="questions[${questionCount}][options][]" class="form-control" required>
-                <label for="questions[${questionCount}][correct_answer]" class="form-label">Correct Option (0 or 1)</label>
-                <input type="number" name="questions[${questionCount}][correct_answer]" class="form-control" required>
-            </div>`;
-        document.getElementById('questions').insertAdjacentHTML('beforeend', newQuestion);
-    });
-</script>
-@endsection --}}
-
-
-
-{{-- @extends('layouts.app')
-
-@section('content')
-<h1>Create a New Quiz</h1>
-<form action="{{ route('quizzes.store') }}" method="POST">
-    @csrf
-    <input type="text" name="title" placeholder="Quiz Title" required>
-    <button type="submit">Create</button>
-</form>
-@endsection --}}
