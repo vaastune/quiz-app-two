2557b0512e7c271b56c4e35164ba38b43220a54d
Ruth <113599167+vaastune@users.noreply.github.com>
2024-12-02 21:32:19 +0700

    a

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Result; // Import the Result model
+use App\Models\Quiz; // Import the Quiz model
 
 class ResultsController extends Controller
 {
