774d5636c493dddadacd593ce59bd7907079c577
Ruth <113599167+vaastune@users.noreply.github.com>
2024-12-03 12:36:15 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,28 @@ use App\Http\Controllers\QuizController;
 use App\Http\Controllers\ResultsController;
 
 Route::middleware('auth')->group(function () {
+    // Quiz Routes
     Route::get('/quizzes', [QuizController::class, 'index'])->name('quizzes.index');
     Route::get('/quizzes/create', [QuizController::class, 'create'])->name('quizzes.create');
     Route::post('/quizzes', [QuizController::class, 'store'])->name('quizzes.store');
-    Route::get('/quizzes/{id}', [QuizController::class, 'show'])->name('quizzes.show');
-    Route::get('/quizzes/{id}/add-questions', [QuizController::class, 'addQuestions'])->name('quizzes.addQuestions');
+    Route::get('/quizzes/{quiz}', [QuizController::class, 'show'])->name('quizzes.show');
     Route::get('/quizzes/{quiz}/take', [QuizController::class, 'take'])->name('quizzes.take');
     Route::post('/quizzes/{quiz}/submit', [QuizController::class, 'submit'])->name('quizzes.submit');
-    Route::post('/quizzes/{id}/complete', [QuizController::class, 'completeQuiz'])->name('quizzes.complete');
-    Route::get('/results', [ResultsController::class, 'index'])->name('results.index');
-    Route::post('/quizzes/{quiz}/add-questions', [QuizController::class, 'storeAdditionalQuestions'])->name('quizzes.storeAdditionalQuestions');
-    Route::get('/quizzes/{quiz}/add-questions', [QuizController::class, 'addQuestions'])->name('quizzes.addQuestions');
-    Route::post('/quizzes/{quiz}/store-additional-questions', [QuizController::class, 'storeAdditionalQuestions'])->name('quizzes.storeAdditionalQuestions');
-    Route::post('/quizzes/{quiz}/complete', [QuizController::class, 'completeQuiz'])->name('quizzes.complete');
-    Route::post('/quizzes/{id}/submit', [QuizController::class, 'submitAnswers'])->name('quizzes.submit');
-    Route::resource('quizzes', QuizController::class);
     Route::delete('/quizzes/{quiz}', [QuizController::class, 'destroy'])->name('quizzes.destroy');
-    Route::get('/quizzes/{quiz}/take', [QuizController::class, 'take'])->name('quizzes.take');
-    Route::post('/quizzes/{quiz}/submit', [QuizController::class, 'submit'])->name('quizzes.submit');
 
+    // Question Management
+    Route::get('/quizzes/{quiz}/add-questions', [QuizController::class, 'addQuestions'])->name('quizzes.addQuestions');
+    Route::post('/quizzes/{quiz}/add-questions', [QuizController::class, 'storeAdditionalQuestions'])->name('quizzes.storeAdditionalQuestions');
 
+    // Results
+    Route::get('/results', [ResultsController::class, 'index'])->name('results.index');
 });
 
+// Home Page and Dashboard
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('welcome');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Authentication Routes
 Auth::routes();
