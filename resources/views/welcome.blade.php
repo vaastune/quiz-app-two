1580e64ca87b4bb271d16af5c5c30611da48084d
Ruth <113599167+vaastune@users.noreply.github.com>
2024-12-03 15:30:52 +0700

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,11 @@
     <p>We're glad to have you here. Enjoy exploring the quizzes!</p>
     <a href="{{ route('quizzes.index') }}" class="btn btn-primary">Start Quizzing</a>
 
-    <footer class="mt-4 text-center">
-        <p>&copy; {{ date('2024') }} All rights reserved.</p>
-    </footer>
+
 
 </div>
 @endsection
+
+<footer class="mt-4 text-center">
+    <p>&copy; {{ date('Y') }} All rights reserved.</p>
+</footer>
