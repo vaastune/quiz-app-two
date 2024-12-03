a75694ff1242e261faebeb5a761f9dcabc4f431b
Ruth <113599167+vaastune@users.noreply.github.com>
2024-12-03 15:23:11 +0700

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,5 +6,10 @@
     <h1>Welcome, {{ Auth::user()->name }}!</h1>
     <p>We're glad to have you here. Enjoy exploring the quizzes!</p>
     <a href="{{ route('quizzes.index') }}" class="btn btn-primary">Start Quizzing</a>
+
+    <footer class="mt-4 text-center">
+        <p>&copy; {{ date('Y') }} All rights reserved.</p>
+    </footer>
+
 </div>
 @endsection
